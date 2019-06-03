2ef82b3d4e801e349088f6cc11be1301ff659dbe
Seth Croston Barber <scbarber@gmail.com>
2019-06-03 15:12:16 -0700

    Shrink the footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,7 +63,11 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
-        <footer>
+        <footer
+          style={{
+            fontSize: "smaller"
+          }}
+        >
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
